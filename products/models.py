962e0100a9323c6cf8097dcaef70d15ddd7fd054
Mares <maresbejo@gmail.com>
2024-11-25 17:33:49 +0700

    [crud] basic crud with ORM and Validation

--- products/models.py
+++ products/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'products'
 
     def __str__(self):
         return self.name
--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import Product
+from django.core.exceptions import ValidationError
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'price', 'stock', 'created_at']
+
+    def validate_price(self, value):
+        """Validasi harga agar lebih dari 0"""
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than 0.")
+        return value
+
+    def validate_stock(self, value):
+        """Validasi stok agar tidak negatif"""
+        if value < 0:
+            raise serializers.ValidationError("Stock cannot be negative.")
+        return value
