021ddd51e01931a36b5e531d1bcae4dfabb86344
Mares <maresbejo@gmail.com>
2024-11-26 14:49:20 +0700

    [deploy] preparation

--- product_backend/settings.py
+++ product_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%)9dv+n&$@j#x)vovp9ut6ee=a#1y+zdywh*y-ui6c6kpdt*gq'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-%)9dv+n&$@j#x)vovp9ut6ee=a#1y+zdywh*y-ui6c6kpdt*gq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -83,9 +84,9 @@ WSGI_APPLICATION = 'product_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'interview_maxy',
-        'USER': 'root',
-        'PASSWORD': 'rahasia',
+        'NAME': config('DB_NAME', default=''),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST': 'localhost',
         'PORT': '3307',
     }
